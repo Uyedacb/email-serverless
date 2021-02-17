9e350bec15aa8893e58a25eaa7e8d4da4ffb829e
Uyedacb <uyedacb@uw.edu>
2021-02-17 15:45:34 -0800

    remove http request, soley nodemailer

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const nodemailer = require("nodemailer");
 const result = require('dotenv').config();
-const got = require('got');
 
 if (result.error) {
   throw result.error;
 }
 
 exports.handler = async function(event) {
-  var accessToken = "";
-  try {
-    const requestAccess = await got.post('https://oauth2.googleapis.com/token',
-      {searchParams:
-        {
-        client_id: process.env.GOOG_CLIENT_ID,
-        client_secret: process.env.GOOG_SECRET_KEY,
-        grant_type: "refresh_token",
-        refresh_token: process.env.GOOG_REFRESH_KEY
-        }
-    });
-    accessToken = requestAccess.body.access_token;
-  } catch (e) {
-    console.log(e);
-  }
   //var messageObj = JSON.parse(event.body);
   messageObj = {sender: "test", email: "test", message: "test"};
   let sender = "<p>From: " + messageObj.sender + "</p>";
@@ -29,31 +13,33 @@ exports.handler = async function(event) {
   let message = messageObj.message;
   const mailOptions = {
     from: "randomemail@email.com",
-    to: "uyedaportfolio@gmail.com",
+    to: "randomemail@email.com",
     subject: "Node.js Email with Secure OAuth",
     generateTextFromHTML: true,
     html: sender + email + message
   };
-  const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-         type: "OAuth2",
-         user: "uyedaportfolio@gmail.com", 
-         clientId:  process.env.GOOG_CLIENT_ID,
-         clientSecret: process.env.GOOG_SECRET_KEY,
-         refreshToken: process.env.GOOG_REFRESH_KEY,
-         accessToken: accessToken
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-  
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
-    smtpTransport.close();
-  });
-  return smtpTransport;
+  try {
+    const smtpTransport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+          type: "OAuth2",
+          user: "randomemail@email.com", 
+          clientId:  process.env.GOOG_CLIENT_ID,
+          clientSecret: process.env.GOOG_SECRET_KEY,
+          refreshToken: process.env.GOOG_REFRESH_KEY,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+      error ? console.log(error) : console.log(response);
+      smtpTransport.close();
+    });
+    return smtpTransport;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 exports.handler();
